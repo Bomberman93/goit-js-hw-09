2bc0a50a5edc3a44db196c30b539880bbbb322e3
Bomberman93 <91456105+Bomberman93@users.noreply.github.com>
2022-01-10 21:08:13 +0200

    dz9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+// const firstButton = document.querySelector('[data-start]');
+// const secondButton = document.querySelector('[data-stop]');
+// const bodyTeg = document.querySelector('body');
 
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// firstButton.addEventListener("click",() => {
+//     timerId = setInterval(() => {
+//       bodyTeg.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//   });
+  
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  const startBtn = document.querySelector("[data-start]");
+  const stopBtn = document.querySelector("[data-stop]");
+  const bodyEl = document.querySelector("body");
+  
+  startBtn.addEventListener("click", () => {
+      timerId = setInterval(() => {
+  
+      startBtn.setAttribute("disabled", "disabled")
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
